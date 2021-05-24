8c0e250245997dc42db085273481363dae1c1fe3
caitl <caitl@KittyBook2>
2021-05-24 13:26:38 -0700

    Continued StringMethods   5/24/21

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,34 +32,56 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		if(s1.length() > s2.length()) {
+		if (s1.length() > s2.length()) {
 			return s1;
 		}
-		
+
 		return s2;
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if (s.contains("underscores"))
+			return s.replace(" ", "_");
+
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		String s1trim = s1.trim();
+		String s2trim = s2.trim();
+		String s3trim = s3.trim();
+
+		int s1i = s1trim.indexOf(" ") + 1;
+		int s2i = s2trim.indexOf(" ") + 1;
+		int s3i = s3trim.indexOf(" ") + 1;
+
+		String s1char = s1trim.substring(s1i);
+		String s2char = s2trim.substring(s2i);
+		String s3char = s3trim.substring(s3i);
+
+		if (s1char.compareTo(s2char) < 0 && s1char.compareTo(s3char) < 0) {
+			return s1trim;
+		}
+
+		else if (s2char.compareTo(s1char) < 0 && s2char.compareTo(s3char) < 0) {
+			return s2trim;
+		}
+
+		return s3trim;
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
+		
 		return 0;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
 		return 0;
@@ -75,13 +97,11 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
 		return 0;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
@@ -90,14 +110,13 @@ public class StringMethods {
 		return 0;
 	}
 
-
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
 		return true;
 	}
-	
+
 }
 
 class Utilities {
